89dca8f10cde63e54adae41bac3f58217501d60a
Kaz Walker <kaz@printtopeer.com>
2014-01-31 16:11:39 -0700

    Updated printing flow to give more feedback

--- burijji/repWrapper.py
+++ burijji/repWrapper.py
@@ -1,22 +1,66 @@
 from printrun.printcore     import printcore
 from printrun               import gcoder
-from machine                import BurijjiMachine
 from time                   import sleep
+from collections            import deque
+import threading
 import re
 
-class repWrapper(BurijjiMachine):
+class repWrapper:
     _temp_exp = re.compile("([TB]\d*):([-+]?\d*\.?\d*)")
     _uuid_exp = re.compile("UUID:([0-F]{8}-[0-F]{4}-4[0-F]{3}-[89AB][0-F]{3}-[0-F]{12})", re.I)
 
     def __init__(self, server):
-        super(repWrapper, self).__init__(server)
-        self.__printer     = printcore()
-        self._machine_info = {'type': 'RepRap', 'model':  'Unknown', 'uuid': None}
+        self._server           = server
+        self._mutex            = threading.Lock()
+        self._temp_subscribers = []
+        self._info_subscribers = []
+        self._raw_subscribers  = []
+        self._temperatures     = {}
+        self._current_line     = None
+        self._printing         = False
+        self._paused           = False
+        self._machine_info     = {}
+        self._routines         = {}
+        self._raw_output       = deque()
+        self._other_messages   = deque()
+        self.__printer         = printcore()
+        self._machine_info     = {'type': 'RepRap', 'model':  'Unknown', 'uuid': None}
+        self._current_segment  = 'none'
+        self._gcode_file       = None
+
+    def start(self):
+        threading.Thread(target=self._update).start()
+        threading.Thread(target=self._run).start()
+
+    def _run(self):
+        while self._server.running:
+            sleep(1)
+
+            self._mutex.acquire()
+            temp_msg         = {'action': 'temperature', 'data': self._temperatures}
+            info_msg         = {'action': 'info', 'data': {'current_line': self._current_line, 'printing': self._printing, 'paused': self._paused, 'machine_info': self._machine_info, 'current_segment': self._current_segment}}
+            temp_subscribers = self._temp_subscribers
+            info_subscribers = self._info_subscribers
+            raw_subscribers  = self._raw_subscribers
+            raw_output       = list(self._raw_output)
+            other_messages   = list(self._other_messages)
+            self._raw_output.clear()
+            self._other_messages.clear()
+            self._mutex.release()
+
+            for fileno in temp_subscribers: self._server.add_to_queue(fileno, temp_msg)
+            for fileno in info_subscribers:
+                self._server.add_to_queue(fileno, info_msg)
+                for message in other_messages:
+                    self._server.add_to_queue(fileno, message)
+            for fileno in raw_subscribers:
+                for line in raw_output:
+                    self._server.add_to_queue(fileno, {'action': 'raw', 'data': line})
 
     def _update(self):
         printer        = self.__printer
         printer.recvcb = self._parse_line
-        printer.endcb  = self._end_print
+        printer.endcb  = self._advance_segment
         printer.connect(self._server.port, self._server.baud)
         sleep(1)
         printer.send_now('M115')
@@ -33,19 +77,150 @@ class repWrapper(BurijjiMachine):
 
         self.__printer.disconnect()
 
+    def machine_info(self, fileno, data):
+        self._server.add_to_queue(fileno, {'action': 'machine_info', 'data': self._machine_info})
+
+    def send_commands(self, fileno, data):
+        if type(data) is list:
+            self._send_commands(data)
+        else:
+            self.bad_data_sent(fileno)
+
+    def print_file(self, fileno, data):
+        self._mutex.acquire()
+        if fileno not in self._info_subscribers: self._info_subscribers.append(fileno)
+        self._mutex.release()
+
+        self.add_other_message({'action': 'print_started', 'data': ''})
+        self._gcode_file = data
+        self._advance_segment()
+
+    def cancel_print(self, fileno, data):
+        self._cancel_print()
+        self.add_other_message({'action': 'print_cancelled', 'data': ''})
+
+    def pause_print(self, fileno, data):
+        if self._printing:
+            self._pause_print()
+            if 'pause_print' in self._routines: self._send_commands(self._routines['pause_print'])
+            self.add_other_message({'action': 'print_paused', 'data': ''})
+
+    def print_complete(self):
+        self.add_other_message({'action': 'print_complete', 'data': ''})
+
+    def resume_print(self, fileno, data):
+        if self._paused:
+            self.add_other_message({'action': 'print_resumed', 'data': ''})
+            if 'resume' in self._routines: self._send_commands(self._routines['resume_print'])
+            self._resume_print()
+
+    def run_routine(self, fileno, data):
+        if data in self._routines:
+            self._send_commands(self._routines[data])
+        else: self._server.add_to_queue(fileno, {'action': 'routine_error', 'data': 'routine not defined'})
+
+    def update_routines(self, fileno, data):
+        if type(data) is dict:
+            for key,val in data.iteritems():
+                if type(val) is not list: return(self.bad_data_sent(fileno))
+            self._routines.update(data)
+        else:
+            self.bad_data_sent(fileno)
+
+    def add_other_message(self, message):
+        self._mutex.acquire()
+        self._other_messages.append(message)
+        self._mutex.release()
+
+    def subscribe(self, fileno, data):
+        subscription = data['type']
+        if subscription not in ['temperature','info','raw','all']:
+            return(self._server.add_to_queue(fileno, {'action': 'data_error', 'data': 'Invalid subscription type.'}))
+
+        self._mutex.acquire()
+        if subscription == 'temperature':
+            self._temp_subscribers.append(fileno)
+        elif subscription == 'info':
+            self._info_subscribers.append(fileno)
+        elif subscription == 'raw':
+            self._raw_subscribers.append(fileno)
+        elif subscription == 'all':
+            self._temp_subscribers.append(fileno)
+            self._info_subscribers.append(fileno)
+            self._raw_subscribers.append(fileno)
+        self._mutex.release()
+
+    def unsubscribe(self, fileno, data):
+        subscription = data['type']
+        if subscription not in ['temperature','info','raw','all']:
+            return(self._server.add_to_queue(fileno, {'action': 'data_error', 'data': 'Invalid subscription type.'}))
+
+        self._mutex.acquire()
+        if subscription == 'temperature':
+            try:
+                self._temp_subscribers.remove(fileno)
+            except:
+                pass
+        elif subscription == 'info':
+            try:
+                self._info_subscribers.remove(fileno)
+            except:
+                pass
+        elif subscription == 'raw':
+            try:
+                self._raw_subscribers.remove(fileno)
+            except:
+                pass
+        else:
+            try:
+                self._temp_subscribers.remove(fileno)
+            except:
+                pass
+            try:
+                self._info_subscribers.remove(fileno)
+            except:
+                pass
+            try:
+                self._raw_subscribers.remove(fileno)
+            except:
+                pass
+        self._mutex.release()
+
+    def bad_data_sent(self, fileno):
+        self._server.add_to_queue(fileno, {'action': 'data_error', 'data': 'Malformed data.'})
+
     def _send_commands(self, commands):
         for command in commands:
             self.__printer.send_now(command)
 
-    def _print_file(self, data):
-        gcode = gcoder.GCode([i.strip() for i in open(data)])
-        self.__printer.startprint(gcode)
+    def _advance_segment(self):
+        if self._current_segment == 'none':
+            self._current_segment = 'starting'
+            if 'start_print' in self._routines:
+                self.__printer.startprint(gcoder.GCode(self._routines['start_print']))
+            else:
+                self._advance_segment()
+
+        elif self._current_segment == 'starting':
+            self._current_segment = 'printing'
+            self.__printer.startprint(gcoder.GCode([i.strip() for i in open(self._gcode_file)]))
+
+        elif self._current_segment == 'printing':
+            self._current_segment = 'ending'
+            if 'end_print' in self._routines:
+                self.__printer.startprint(gcoder.GCode(self._routines['end_print']))
+            else:
+                self._advance_segment()
+
+        elif self._current_segment == 'ending':
+            self.print_complete()
+            self._current_segment = 'none'
 
     def _end_print(self):
         if 'end_print' in self._routines: self._send_commands(self._routines['end_print'])
         self.print_complete()
 
-    def _stop_print(self):
+    def _cancel_print(self):
         self.__printer.pause()
         self._end_print()
 
